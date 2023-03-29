1156ddd5b21d42b53720dd0c6da3324c79de87c4
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-29 17:34:17 +0530

    Added:code to load the Indian States code information from csv file

--- main/java/censusanalyser/IndiaCensusCSV.java
+++ main/java/censusanalyser/IndiaCensusCSV.java
@@ -4,25 +4,25 @@ import com.opencsv.bean.CsvBindByName;
 
 public class IndiaCensusCSV {
 
-    @CsvBindByName(column = "State", required = true)
-    public String state;
+    @CsvBindByName(column = "SrNo", required = true)
+    public int srNo;
 
-    @CsvBindByName(column = "Population", required = true)
-    public int population;
+    @CsvBindByName(column = "State Name", required = true)
+    public String stateName;
 
-    @CsvBindByName(column = "AreaInSqKm", required = true)
-    public int areaInSqKm;
+    @CsvBindByName(column = "TIN", required = true)
+    public int tin;
 
-    @CsvBindByName(column = "DensityPerSqKm", required = true)
-    public int densityPerSqKm;
+    @CsvBindByName(column = "StateCode", required = true)
+    public String stateCode;
 
     @Override
     public String toString() {
         return "IndiaCensusCSV{" +
-                "State='" + state + '\'' +
-                ", Population='" + population + '\'' +
-                ", AreaInSqKm='" + areaInSqKm + '\'' +
-                ", DensityPerSqKm='" + densityPerSqKm + '\'' +
+                "srNo=" + srNo +
+                ", stateName='" + stateName + '\'' +
+                ", tin=" + tin +
+                ", stateCode='" + stateCode + '\'' +
                 '}';
     }
 }
